e28acb9cebb01acb295728248654c6e7c59623be
Amit Breiman <crimson.breiman@gmail.com>
2024-05-29 14:26:59 +0300

    connection

--- Assets/_Scripts/MainMenu/ConnectionManager.cs
+++ Assets/_Scripts/MainMenu/ConnectionManager.cs
@@ -6,7 +6,15 @@ using Photon.Realtime;
 
 public class ConnectionManager : MonoBehaviourPunCallbacks
 {
+    public List<RoomInfo> RoomList { get; private set; }
 
+
+    public bool IsConnected { get; private set; } = false;
+
+    private void Awake()
+    {
+        RoomList = new List<RoomInfo>();
+    }
     private void Start()
     {
         PhotonNetwork.ConnectUsingSettings();
@@ -15,11 +23,37 @@ public class ConnectionManager : MonoBehaviourPunCallbacks
     public override void OnConnectedToMaster()
     {
         base.OnConnectedToMaster();
+        PhotonNetwork.JoinLobby();
+        IsConnected = true;
+    }
+    public override void OnRoomListUpdate(List<RoomInfo> roomList)
+    {
+        base.OnRoomListUpdate(roomList);
+
+        UpdateRoomList(roomList);
+        
+    }
+    private void UpdateRoomList(List<RoomInfo> updatedRooms)
+    {
+        foreach (RoomInfo room in updatedRooms)
+        {
+            if (RoomList.Contains(room))
+            {
+                // Remove room from list if not listed anymore.
+                if(room.RemovedFromList)
+                    RoomList.Remove(room);
+            }
+            else
+            {
+                // Add the new room to the list.
+                RoomList.Add(room);
+            }
+        }
     }
 
     public void CreateRoom()
     {
-        PhotonNetwork.CreateRoom("A Room");
+        PhotonNetwork.CreateRoom(System.DateTime.Now.ToString());
     }
 
     public override void OnCreatedRoom()
@@ -31,6 +65,7 @@ public class ConnectionManager : MonoBehaviourPunCallbacks
     public override void OnCreateRoomFailed(short returnCode, string message)
     {
         base.OnCreateRoomFailed(returnCode, message);
+        Debug.Log("Creating room failed");
     }
 
     public void JoinRandomRoom()
@@ -46,20 +81,20 @@ public class ConnectionManager : MonoBehaviourPunCallbacks
     public override void OnJoinedRoom()
     {
         base.OnJoinedRoom();
+        Debug.Log("Joined room");
     }
     public override void OnJoinRoomFailed(short returnCode, string message)
     {
         base.OnJoinRoomFailed(returnCode, message);
+        Debug.Log("failed joining room");
+
     }
     public override void OnJoinRandomFailed(short returnCode, string message)
     {
         base.OnJoinRandomFailed(returnCode, message);
     }
 
-    public override void OnRoomListUpdate(List<RoomInfo> roomList)
-    {
-        base.OnRoomListUpdate(roomList);
-    }
+    
 
 
 
