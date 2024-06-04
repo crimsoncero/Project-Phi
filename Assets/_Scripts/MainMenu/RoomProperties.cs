2497574177aa3935820a884c3d78349a3d97f8aa
Amit Breiman <93070078+crimsoncero@users.noreply.github.com>
2024-06-04 18:18:13 +0300

    1

---
+++ Assets/_Scripts/MainMenu/RoomProperties.cs
@@ -0,0 +1,95 @@
+﻿
+
+using Photon.Realtime;
+using UnityEngine;
+
+public class RoomProperties
+{
+    public ExitGames.Client.Photon.Hashtable Hashtable { get; private set; }
+
+    private int _playerCount = 4;
+    private Maps _map = Maps.Random;
+    private int _matchTime = 120;
+    private int _scoreGoal = 0;
+    private string _name = "Match";
+    private WeaponSpawnPattern _weaponSpawnPattern = WeaponSpawnPattern.Normal;
+
+    
+    public RoomProperties()
+    {
+        Hashtable = new ExitGames.Client.Photon.Hashtable()
+        {
+            { "m", _map },
+            { "t", _matchTime },
+            { "n", _name },
+            { "s", _scoreGoal },
+            { "w", _weaponSpawnPattern },
+            { "p", _playerCount }
+        };
+    }
+
+    public RoomProperties(ExitGames.Client.Photon.Hashtable hashtable)
+    {
+        Hashtable = hashtable;
+    }
+
+
+    public int PlayerCount
+    {
+        get { return _playerCount; }
+        set
+        {
+            _playerCount = Mathf.Clamp(value, 1, 6);
+            Hashtable["p"] = _playerCount;
+        }
+    }
+    public Maps Map
+    {
+        get { return _map; }
+        set
+        {
+            _map = value;
+            Hashtable["m"] = _map;
+        }
+    }
+    public int MatchTime
+    {
+        get { return _matchTime; }
+        set
+        {
+            _matchTime = value;
+            Hashtable["t"] = _matchTime;
+        }
+    }
+    public int ScoreGoal
+    {
+        get { return _scoreGoal; }
+        set
+        {
+            _scoreGoal = value;
+            Hashtable["s"] = _scoreGoal;
+        }
+    }
+    public string Name
+    {
+        get { return _name; }
+        set
+        {
+            _name = value;
+            Hashtable["n"] = _name;
+        }
+    }
+    public WeaponSpawnPattern WeaponSpawnPattern
+    {
+        get { return _weaponSpawnPattern; }
+        set
+        {
+            _weaponSpawnPattern = value;
+            Hashtable["w"] = _weaponSpawnPattern;
+        }   
+    }
+
+
+
+
+}
--- Assets/_Scripts/MainMenu/RoomSetup.cs
+++ Assets/_Scripts/MainMenu/RoomSetup.cs
@@ -27,91 +27,20 @@ public class RoomSetup : ScriptableObject
     [Tooltip("If a client disconnects, this actor is inactive first and removed after this timeout. In milliseconds.")]
     [SerializeField] private int _playersTtl = 5_000;
     
-    [Header("Room Properties")]
-    [SerializeField, Range(1, 6)] private int _playerCount = 4;
-    [SerializeField] private Maps _map = Maps.Random;
-    [SerializeField] private int _matchTime = 120;
-    [SerializeField] private int _scoreGoal = 0;
-    [SerializeField] private string _name = "Match";
-    [SerializeField] private WeaponSpawnPattern _weaponSpawnPattern = WeaponSpawnPattern.Normal;
-
-    public int PlayerCount
-    {
-        get { return _playerCount; }
-        set
-        {
-            _playerCount = Mathf.Clamp(value, 1, 6);
-            RoomOptions.MaxPlayers = _playerCount;
-        }
-    }
-    public Maps Map
-    {
-        get { return _map; }
-        set
-        {
-            _map = value;
-            RoomOptions.CustomRoomProperties["m"] = _map;
-        }
-    }
-    public int MatchTime
-    {
-        get { return _matchTime; }
-        set
-        {
-            _matchTime = value;
-            RoomOptions.CustomRoomProperties["t"] = _matchTime;
-        }
-    }
-    public int ScoreGoal
-    {
-        get { return _scoreGoal; }
-        set
-        {
-            _scoreGoal = value;
-            RoomOptions.CustomRoomProperties["s"] = _scoreGoal;
-        }
-    }
-    public string Name
-    {
-        get { return _name; }
-        set
-        {
-            _name = value;
-            RoomOptions.CustomRoomProperties["n"] = _name;
-        }
-    }
-    public WeaponSpawnPattern WeaponSpawnPattern
-    {
-        get { return _weaponSpawnPattern; }
-        set
-        {
-            _weaponSpawnPattern = value;
-            RoomOptions.CustomRoomProperties["w"] = _weaponSpawnPattern;
-        }
-    }
+    public RoomProperties RoomProperties { get; set; }
 
     public RoomSetup(string roomID)
     {
         RoomID = roomID;
-        
+        RoomProperties = new RoomProperties();
         RoomOptions = new RoomOptions();
         RoomOptions.PlayerTtl = _playersTtl;
         RoomOptions.EmptyRoomTtl = 0;
-        RoomOptions.MaxPlayers = _playerCount;
-        SetHashtable();
+        RoomOptions.MaxPlayers = RoomProperties.PlayerCount;
+        RoomOptions.CustomRoomProperties = RoomProperties.Hashtable;
     }
 
-    private void SetHashtable()
-    {
-        RoomOptions.CustomRoomProperties = new ExitGames.Client.Photon.Hashtable()
-        {
-            { "m", _map },
-            { "t", _matchTime },
-            { "n", _name },
-            { "s", _scoreGoal },
-            { "w", _weaponSpawnPattern }
-        };
-    }
+
 
 
 
