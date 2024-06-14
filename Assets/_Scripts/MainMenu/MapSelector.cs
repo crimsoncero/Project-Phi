ced348d749a5e9022676713ce2a6eaf8a4d4b5c0
yoavk-sudo <kenkendler123@gmail.com>
2024-06-15 01:26:28 +0300

    Created weapon selector

--- Assets/_Scripts/MainMenu/MapSelector.cs
+++ Assets/_Scripts/MainMenu/MapSelector.cs
@@ -3,25 +3,7 @@ using UnityEngine;
 using TMPro;
 using System;
 
-public class MapSelector : MonoBehaviour
+public class MapSelector : EnumSelector<Maps>
 {
-    [SerializeField] TMP_Text _mapText;
-    [SerializeField] private Maps _maps;
-
-    private void Start()
-    {
-        ChangeMapText();
-    }
-
-    public void ChangeMap(int value = 1)
-    {
-        int mapCount = Enum.GetValues(typeof(Maps)).Length;
-        _maps = (Maps)(((int)_maps + value + mapCount) % mapCount);
-        ChangeMapText();
-    }
-
-    private void ChangeMapText()
-    {
-        _mapText.text = _maps.ToString();
-    }
+    
 }
