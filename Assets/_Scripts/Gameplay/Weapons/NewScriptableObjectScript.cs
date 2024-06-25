bc97757a32d4435b107b752c043624e1391a3e8a
Amit Breiman <93070078+crimsoncero@users.noreply.github.com>
2024-06-25 20:47:32 +0300

    1

--- Assets/_Scripts/Gameplay/Weapons/NewScriptableObjectScript.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "NewScriptableObjectScript", menuName = "Scriptable Objects/NewScriptableObjectScript")]
-public class NewScriptableObjectScript : ScriptableObject
-{
-    
-}
