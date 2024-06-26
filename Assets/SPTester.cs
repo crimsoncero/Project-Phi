9c54b422f652d7b0b614983b1aa23d3fe33c5609
Amit Breiman <crimson.breiman@gmail.com>
2024-06-26 09:44:28 +0300

    1

--- Assets/SPTester.cs
+++ Assets/SPTester.cs
@@ -1,13 +1,30 @@
+using Photon.Pun;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class SPTester : MonoBehaviour
+public class SPTester : MonoBehaviourPunCallbacks
 {
     [SerializeField] private GameObject _player;
     
     private PlayerController _playerController;
-
     private void Start()
+    {
+        PhotonNetwork.OfflineMode = true;
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        base.OnConnectedToMaster();
+        PhotonNetwork.CreateRoom("SinglePlayer");
+    }
+
+    public override void OnJoinedRoom()
+    {
+        base.OnJoinedRoom();
+        Init();
+    }
+
+    private void Init()
     {
         _playerController = _player.GetComponent<PlayerController>();
         _playerController.enabled = true;
