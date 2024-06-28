4c67cdb805e363f04fa3d919770e1c210fbd863a
Amit Breiman <crimson.breiman@gmail.com>
2024-06-28 14:14:44 +0300

    1

--- Assets/_Scripts/Gameplay/Weapons/Projectile.cs
+++ Assets/_Scripts/Gameplay/Weapons/Projectile.cs
@@ -1,4 +1,6 @@
 using Photon.Realtime;
+using System;
+using System.Collections;
 using UnityEngine;
 
 public class Projectile : MonoBehaviour
@@ -6,9 +8,11 @@ public class Projectile : MonoBehaviour
     public Player Owner { get; private set; }
     public int Damage { get; private set; }
 
-    [SerializeField] private Rigidbody2D _rigidbody;
+    [Tooltip("The maximum distance to snap the projectile to its expected position.")]
+    [SerializeField] private float _distanceToSnap = 0.1f;
+    [SerializeField] private Rigidbody2D _rigidbody2D;
 
-
+    [SerializeField] private float _multiplier = 0.7f;
 
     public void Init(Player owner, int damage, float velocity, Quaternion shipRotation, float lag)
     {
@@ -19,7 +23,35 @@ public class Projectile : MonoBehaviour
 
         transform.up = originalDirection;
 
-        _rigidbody.velocity = originalDirection * velocity;
-        _rigidbody.position += _rigidbody.velocity * lag;
+        StartCoroutine(LatencyCatchup(originalDirection * velocity, transform.position, lag));
+    }
+
+
+    private IEnumerator LatencyCatchup(Vector2 baseVelocity, Vector2 startingPosition, float lag)
+    {
+
+        while (true)
+        {
+            // Calculate the expected position of the projectile based on latency, and the distance from the current position.
+            Vector3 expectedPosition = startingPosition + (baseVelocity * lag);
+            float distanceFromExpected = Vector3.Distance(expectedPosition, transform.position);
+
+            // Snap the position and end the catchup if below the threshold. 
+            if(distanceFromExpected < _distanceToSnap)
+            {
+                _rigidbody2D.velocity = baseVelocity;
+                transform.position = expectedPosition;
+                yield break;
+            }
+
+            float velocityMultiplier = Mathf.Log(_multiplier * distanceFromExpected + MathF.E, MathF.E);
+
+            _rigidbody2D.velocity = baseVelocity * velocityMultiplier;
+
+
+            // Wait for next frame and aggregate the lag.
+            yield return null;
+            lag += Time.deltaTime;
+        }
     }
 }
